01d884748de5b80f1591ebc95e9a32022f4bc6c0
ArtemDrygin <artem.drygin.1995@gmail.com>
2022-12-07 04:38:26 +0400

    * Head overloading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
+import Head from "next/head";
 
 export default function App({Component, pageProps}: AppProps): JSX.Element {
-    return <Component {...pageProps} />;
+    return <div>
+        <Head>
+            <title>Create Next</title>
+            <link key={1} rel="icon" href="/favicon2.ico"/>
+        </Head>
+        <Component {...pageProps} />
+    </div>;
 }
