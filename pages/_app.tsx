e730a174ef542f50d0e71ddada4ecc87e4ae59a1
ArtemDrygin <artem.drygin.1995@gmail.com>
2022-12-07 04:52:02 +0400

    * add _document.tsx to add lang "ru"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import Head from "next/head";
+import React from "react";
 
 export default function App({Component, pageProps}: AppProps): JSX.Element {
-    return <div>
+    return <>
         <Head>
-            <title>Create Next</title>
-            <link key={1} rel="icon" href="/favicon2.ico"/>
+            <title>My Top - our best top</title>
+            <link rel="icon" href="/favicon.ico"/>
         </Head>
         <Component {...pageProps} />
-    </div>;
+    </>;
 }
